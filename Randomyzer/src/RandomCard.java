71b8c22fbdac4e1826b8929723def5e76fcb775b
Tyler Tucker <tuc13006@byui.edu>
2019-07-20 12:55:27 -0600

    Update RandomCard.java

--- Randomyzer/src/RandomCard.java
+++ Randomyzer/src/RandomCard.java
@@ -5,16 +5,21 @@
  * playing cards.
  */
 
-
+// extends base number class
  public class RandomCard extends BaseNumber{
     /* Instance Variables */
+
+// sets up array for suits and card numbers/names
     public String Result(){
         String suits[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
         String cards[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
                           "Jack", "Queen", "King", "Ace" };
+
+// object instance for range 
         BaseNumber range = new BaseNumber();
         BaseNumber range2 = new BaseNumber();
-        
+
+// return cards for both
        return cards[range.rand(13)]
             + " of " +suits[range2.rand(4)];
     }
