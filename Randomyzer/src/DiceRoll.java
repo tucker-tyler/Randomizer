19b0461c6efb84ca0441c165fcafb9fd0edc862c
Tyler Tucker <tuc13006@byui.edu>
2019-07-20 12:58:45 -0600

    Update DiceRoll.java

--- Randomyzer/src/DiceRoll.java
+++ Randomyzer/src/DiceRoll.java
@@ -1,15 +1,20 @@
 /* Takes numbers 1-6 and prints it out. Just like a dice roll.
 */
 
+// extends base number
 public class DiceRoll extends BaseNumber{
 
 	public String Result() {
-		
+
+// new instance of range
 		BaseNumber range = new BaseNumber();
+
+// set up number 1-6 for the roll
 	    int n = range.rand(6);
 
     n += 1;
-		
+
+// return dice roll
 		return "Your Dice Roll is: " + n;
 	}
 }
