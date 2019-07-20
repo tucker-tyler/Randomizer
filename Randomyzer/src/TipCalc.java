1a435983a159b90e4f760a2ae6ae4902477c1e3d
Tyler Tucker <tuc13006@byui.edu>
2019-07-20 13:04:20 -0600

    Update TipCalc.java

--- Randomyzer/src/TipCalc.java
+++ Randomyzer/src/TipCalc.java
@@ -1,8 +1,9 @@
 /* Takes the input of what percent tip and what the bill costs
  * Prints out the info to the user
  * Offers to quit or restart
- * @TylerTucker
 */
+
+// import scanner
 
 import java.util.Scanner;
 
@@ -11,13 +12,16 @@ public class TipCalc {
 	
 	public String Result() {
 		
-		
+/* set up tip amount and total amount */
+
 		double tip_amount;
 		
 		double total_amount;
-		
+
+// new scanner instance
 		Scanner input = new Scanner(System.in);
-        
+
+// print to prompt user
 		System.out.print("Please enter the meal amount ");
 				
 				double meal_amount = input.nextDouble();
@@ -32,6 +36,8 @@ public class TipCalc {
 		
 		total_amount = meal_amount + tip_amount;
 		input.close();
+
+// return tip, tip amount, total amount
 		return "Tip: $" + tip_amount + "\nTotal: $" + total_amount;
 		
 		
